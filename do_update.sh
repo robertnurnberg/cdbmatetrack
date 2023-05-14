2d28c4900096ddd56ccc1482129cd57b741e6b6e
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-05-14 14:20:55 +0200

    include graphics

--- do_update.sh
+++ do_update.sh
@@ -2,7 +2,9 @@
 
 python3 ../cdblib/fens2cdb.py ChestUCI_23102018.epd > ChestUCI_23102018_cdbeval.epd
 python3 cdbmatetrack.py >> cdbmatetrack.csv
+python3 plotdata.py
 
 git add ChestUCI_23102018_cdbeval.epd cdbmatetrack.csv
-git diff --staged --quiet || git commit -m "update data"
+git add cdbmatetrack.png cdbmatetrackall.png
+git diff --staged --quiet || git commit -m "update data and plots"
 git push origin main >& push.log
